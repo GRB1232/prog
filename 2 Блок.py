6b1bbd63941375e7f2f3e360b100bf56e7c73130
GRB1232 <148261867+GRB1232@users.noreply.github.com>
2023-12-28 14:50:35 +0300

    2 Блок.py

--- 2 Блок.py
+++ 2 Блок.py
@@ -1,8 +1,11 @@
-
-def find_longest_word(file_name):
-    with open(file_name, 'r') as file:
-        text = file.read()
+def find_longest_word(slova):
+    with open(file_name, 'r') as slova:
+        text = slova.read()
         words = text.split()
         max_length = max(len(word) for word in words)
         longest_words = [word for word in words if len(word) == max_length]
         return max_length, longest_words
+
+file_name = 'slova.txt'
+length, longest_words = find_longest_word(file_name)
+print("Самое длинное слово имеет длину", length, "и это слово(а):", longest_words)
